cd3c2052f8aca6018e3e251291881a2d7d6475c7
Wassim <73005040+Jniouen-Wassim@users.noreply.github.com>
2026-07-07 04:35:14 +0200

    fix hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className={`${sora.variable} ${inter.variable} bg-background`}>
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased" suppressHydrationWarning>
         {children}
       </body>
     </html>
